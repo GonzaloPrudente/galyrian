4b804fdcff9e201b8865973040e2478dffefccfd
Eduardo Grajales Villanueva <edymanonek@hotmail.com>
2016-08-13 19:57:44 -0500

    Sitio Web Completo
    Ahora ya se cuenta con un sitio web decente.
    
    Se añadió la licencia y se modificó el sistema de creación de archivos
    js y css.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ stylus = require('gulp-stylus'),
 server = require('gulp-webserver'),
 rename = require('gulp-rename'),
 zip = require('gulp-zip'),
+header = require('gulp-header'),
 uglify = require('gulp-uglify');
 
 var version = '1.0.2'
@@ -22,7 +23,7 @@ const paths = {
 			src: [
 				'./src/js/primero/jquery.js', 
 				'./src/js/primero/hammer.js',
-				'./src/js/*.js'
+				'./dist/galyrian.min.js'
 			],
 			dest: './build/js/',
 			watch: './src/js/**/*.js'
@@ -62,12 +63,15 @@ gulp.task('build:css', function () {
 			suffix: '.min',
 			extname: '.css'
 		}))
+		.pipe(header(banner, { version: version }))
 		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('build:js', function () {
+gulp.task('build:js', ['pre:js', 'post:js']);
+
+gulp.task('post:js', function () {
 	return gulp.src(paths.dev.js.src)
-		.pipe(concat('main.js'))
+		.pipe(concat('post.js'))
 		.pipe(gulp.dest(paths.dev.js.dest))
 		.pipe(uglify())
 		.pipe(rename({
@@ -83,7 +87,7 @@ gulp.task('build:js', function () {
 		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('dist:js', function () {
+gulp.task('pre:js', function () {
 	return gulp.src('./src/js/*.js')
 		.pipe(concat('main.js'))
 		.pipe(uglify())
@@ -92,6 +96,7 @@ gulp.task('dist:js', function () {
 			suffix: '.min',
 			extname: '.js'
 		}))
+		.pipe(header(banner, { version: version }))
 		.pipe(gulp.dest('./dist/'));
 });
 
@@ -128,6 +133,8 @@ gulp.task('watch', function () {
 
 gulp.task('build', ['build:css', 'build:js', 'build:html']);
 
-gulp.task('dist', ['dist:js', 'dist:zip:pnp', 'dist:zip:oc'])
+gulp.task('dist', ['dist:zip:pnp', 'dist:zip:oc'])
 
-gulp.task('default', ['server', 'build', 'dist', 'watch']);
+gulp.task('default', ['server', 'build', 'watch']);
+
+var banner = ' /**\n * Galyrian \n * Versión: ' + version + '\n * Git Repository: https://github.com/EdGraVill/galyrian \n * Algunos Derechos Reservados por {innCode} - 2016 \n * Licencia: GPL 3.0 ----- \n * This file is part of Galyrian ' + version + '. \n * Galyrian is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. \n * Galyrian is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. \n * You should have received a copy of the GNU General Public License along with Foobar. If not, see https://www.gnu.org/licenses/gpl.html\n */\n\n\n'
